06fb375abaad97f95515ccfe8556b5d084c2c1dd
sridharthiru89 <sridharbeaero@gmail.com>
2020-12-09 17:43:31 +0530

    included subtraction funtion and some changes

--- Calc3.py
+++ Calc3.py
@@ -1,10 +1,19 @@
 def add(*x):
-  result = 0
-  for i in x:
-    result+=i
-  return result
+	result = 0
+	for i in x:
+		result+=i
+	return result
+
+
+def sub(*x):
+	result = 0
+	for i in x:
+		result-=i
+	return result
 
   
 if __name__=='__main__':
+	#Below are the two test cases for the written functions
 	print(add(5,10,15,0))
-	print('End!')
+	print(sub(5,5,10,100))
+	
