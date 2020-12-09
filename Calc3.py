bc21b292c6350b9a81368f4a54c6290bb076f263
sridharthiru89 <sridharbeaero@gmail.com>
2020-12-09 15:35:18 +0530

    bugs fixed

--- Calc3.py
+++ Calc3.py
@@ -1,8 +1,9 @@
 def add(*x):
   result = 0
-  for i in *x:
+  for i in x:
     result+=i
   return result
+
   
-  if __name__=='__main__':
-    print(add(5,10,15,20))
+if __name__=='__main__':
+	print(add(5,10,15,0))
